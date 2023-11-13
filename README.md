4017f6c2e725253efd519bb0e7c3046df08903d3
AndreaGul <andreagulino99@gmail.com>
2023-11-13 13:54:04 +0100

    feat: lista della spesa con il ciclo while

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Lista della spesa
 
 Ricreaiamo una lista della spesa con il ciclo while
+
+1 Creamo un elemento ul nell'html con classe lista
+2 Lo richiamiamo nel js
+3 Ci creiamo un array con gli elemente della spesa
+4 Per inserire gli lementi della spesa nell'html inizializiamo un ciclo while:
+dichiariamo una variabile 'i' che useremo come contatore come condizione del ciclo inpostiamo che 'i' deve essere minore della lunchezza dell'array spesa
+e nel ciclo incrementiamo i di 1
+
+5 Creaiamo un elemento li
+6 Nell'elemento li inseriamo l'elemento in posizione i dell'array spesa
+7 Appendiamo li al ul con classe lista
--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//Richiamiamo l'elemento ul nell'html con classe lista
+const lista = document.querySelector('.lista');
+
+//Ci creiamo un array con gli elemente della spesa
+const spesa = ['pane', 'pasta', 'pomodori', 'hamburger', 'acqua', 'insalata'];
+
+//Dichiariamo una variabile 'i' che useremo come contatore
+let i = 0;
+
+//Come condizione del ciclo inpostiamo che 'i' deve essere minore della lunchezza dell'array spesa
+while (i < spesa.length) {
+  //creaiamo un elemento li
+  const li = document.createElement('li');
+
+  //nell'elemento li inseriamo l'elemento in posizione i dell'array spesa
+  li.textContent = spesa[i];
+
+  //appendiamo li al ul con classe lista
+  lista.append(li);
+
+  //nel ciclo incrementiamo i di 1
+  i++;
+}
+console.log(lista);
